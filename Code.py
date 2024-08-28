00bf46ba0938461c7f7ad9d3bec4598f5b4d1195
Himanshu Katwal <himanshukatwalnew@gmail.com>
2024-08-28 14:56:39 +0530

    Netflix EDA code.py

--- Code.py
+++ Code.py
@@ -24,7 +24,20 @@ df.drop_duplicates(subset=["title"],inplace=True)
 df['date_added']=pd.to_datatime(df['data_added'])
 df.dtype #to check if data type of date_added has changed
 
-#Let's analyse trhe Data now
+#Let's analyse the Data now
+type_counts=df['type'].value_counts()
+#since above is a Series object lets convert it into a df
+df1=pd.DataFrame({'type':['Movie','TV Show'],'count':[6125,2663]})
+
+
+#Visualizing the Dsitribution of Movies and TV Shows 
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(10,6))
+sns.barplot(x='type',y='count',data=df1,palette='pastel',width=0.5)
+plt.title('DISTRIBUTION OF MOVIES & TV SHOWS')
+plt.xlabel('Genre')
+plt.ylabel('Distirbution')
+plt.show()
 
 
 

107c5e71d7778f4a0f623e0a90dd4b3b6803dfa9
Himanshu Katwal <himanshukatwalnew@gmail.com>
2024-08-29 16:10:17 +0530

    Update Netflix_EDA.py

--- Netflix_EDA.py
+++ Netflix_EDA.py
@@ -67,6 +67,7 @@ plt.title('Most common Genres on Netflix')
 plt.xlabel('Distribution')
 plt.ylabel('Genres')
 plt.show()
+
 #3.Visualizing the top 10 Directors with Maximum movies
 plt.figure(figsize=(10, 6))
 sns.barplot(x=director_movie_count.values, y=director_movie_count.index, palette='pastel',hue=(director_movie_count))
@@ -75,3 +76,55 @@ plt.xlabel('Number of Movies')
 plt.ylabel('Director')
 plt.show()
 
+#4.Visualizing Movies and Tv Shows launched by Months
+
+#Lets retrieve a list of months from the date_added column and count the number of movies and tv shows separately
+month_added=df['date_added'].dt.month
+df['month_added']=month_added #creating a df of the list of months
+
+movie_counts=df[df['type']=='Movie']['month_added'].value_counts().sort_index()
+tv_counts=df[df['type']=='TV Show']['month_added'].value_counts().sort_index()
+
+plt.figure(figsize=(12, 9))
+plt.plot(movie_counts.index,movie_counts.values,label='Movie')
+plt.plot(tv_counts.index,tv_counts.values,label='TV Show')
+plt.xlabel("Months")
+plt.ylabel("Frequency of releases")
+plt.xticks(range(1, 13), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec'])
+plt.legend()
+plt.grid(True)
+plt.suptitle("Monthly releases of Movies and TV shows on Netflix")
+plt.show()
+
+#5.Visualizing the count of number of Movie entries every year 
+
+#Lets retrieve a list of years from the date_added column and count the number of years
+year_added=df['date_added'].dt.year
+df['year_added']=year_added
+year_counts=df['year_added'].value_counts().sort_index()
+
+plt.figure(figsize=(10, 8))
+bars = plt.bar(year_counts.index, year_counts.values, color=sns.color_palette('pastel', n_colors=len(year_counts)))
+
+# Set y-axis to logarithmic scale
+plt.yscale('log')
+
+plt.title('Count Of Movies Added By Year (Logarithmic Scale)', fontsize=16)
+plt.xlabel('Year Added', fontsize=14)
+plt.ylabel('Count (Log Scale)', fontsize=14)
+
+# Add labels on top of bars
+for bar in bars:
+    height = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2.0, height, int(height), ha='center', va='bottom', fontsize=12)
+
+plt.show()
+
+#6.Visualizing the frequency of common words used in movie titles using tag_cloud or wordcloud plot
+movie_title = ' '.join(df['title'].dropna()) 
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(movie_title)
+plt.figure(figsize=(10, 6))
+plt.imshow(wordcloud, interpolation='spline16')
+plt.axis('off')
+plt.title('Word Cloud of Movie Titles on Netflix')
+plt.show()
